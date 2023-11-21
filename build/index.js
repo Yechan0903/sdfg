f64b70797cd4d7d6de6b557780d3eee0fd827f8b
Yechan0903 Shin <oolo9876@naver.com>
2023-11-21 15:06:13 +0000

    blockchain

--- build/index.js
+++ build/index.js
@@ -1,1 +1,40 @@
-const hello = () => "hi";
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto_1 = __importDefault(require("crypto"));
+class Block {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
+        this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto_1.default.createHash("sha256").update(toHash).digest("hex");
+    }
+}
+class Blockchain {
+    constructor() {
+        this.block = [];
+    }
+    getprevHash() {
+        if (this.block.length === 0)
+            return "";
+        return this.block[this.block.length - 1].hash;
+    }
+    addBlock(data) {
+        const newBlock = new Block(this.getprevHash(), this.block.length + 1, data);
+        this.block.push(newBlock);
+    }
+    getBlocks() {
+        return this.block;
+    }
+}
+const blockchain = new Blockchain();
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+console.log(blockchain.getBlocks());
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,48 @@
-const hello = () => "hi";
+import crypto from "crypto";
+
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+}
+class Block implements BlockShape {
+    public hash: string;
+    constructor(
+        public prevHash:string,
+        public height:number,
+        public data:string
+    ){
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash:string, height:number, data:string){
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
+    }
+}
+
+class Blockchain {
+    private block: Block[];
+    constructor() {
+        this.block = [];
+    }
+    private getprevHash() {
+        if (this.block.length === 0) return "";
+        return this.block[this.block.length - 1].hash;
+    }
+    public addBlock(data:string){
+        const newBlock = new Block(this.getprevHash(), this.block.length + 1, data);
+        this.block.push(newBlock);
+    }
+    public getBlocks(){
+        return [...this.block];
+    }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+
+console.log(blockchain.getBlocks());
